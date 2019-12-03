d21bb16dec9881ebddd61afd9f8942ceb0698f1a
Carlos Prieto <carlosprieto@google.com>
2019-12-03 11:34:16 -0500

    changed configuration to docker compose

--- vsextentions/ms-python.python-2019.4.12954/languageServer.0.2.74/waXyxjfs5a1NNPi03IoqbBYnMlVcP90d0pr8g3PWJYA=/_opcode.cpython-37m-darwin.pyi
+++
@@ -1,8 +0,0 @@
-__doc__ = 'Opcode support module.'
-__file__ = '/Users/carlosprieto/Desarrollo/git/gweb-uniblog-author-prod/venv/lib/python3.7/lib-dynload/_opcode.cpython-37m-darwin.so'
-__name__ = '_opcode'
-__package__ = ''
-def stack_effect(opcode, oparg):
-    'Compute the stack effect of the opcode.'
-    pass
-
